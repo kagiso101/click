265fd3f275e534ff5db4c21ec9c366e6453aaa6f
kagiso101 <hadebekagiso3@gmail.com>
2020-09-25 01:03:12 +0200

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+var clickBtn = document.querySelector(".btn")
+var msg = document.querySelector('.message')
+
+
+function clicked() {
+
+    msg.innerHTML = "Have A Good Day Kagiso! You've Got This!!!!!!!!!!!!"
+    
+}
+
+clickBtn.addEventListener('click', clicked)
