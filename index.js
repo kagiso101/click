e95bbcbf7ba23352828a063528e910c62d8e1b73
kagiso101 <hadebekagiso3@gmail.com>
2020-10-18 21:20:53 +0200

    added time out

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var msg = document.querySelector('.message')
 function clicked() {
 
     msg.innerHTML = "Have A Good Day Kagiso! You've Got This!!!!!!!!!!!!"
-    
+    setTimeout(function () {
+        msg.innerHTML = ""
+    }, 4000)
 }
-
 clickBtn.addEventListener('click', clicked)
